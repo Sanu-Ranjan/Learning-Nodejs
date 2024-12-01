6a0bb6dff576f0d375031f64b8f69eb83e81de6d
Sanu-Ranjan <ranjan.code33@gmail.com>
2024-12-01 20:19:28 +0530

    creating_and_loading_module

---
+++ getting_started/app.js
@@ -0,0 +1,3 @@
+const log = require("./logger");
+
+log.display(135);
---
+++ getting_started/logger.js
@@ -0,0 +1,6 @@
+function display(value) {
+  console.log(`the value is ${value}`);
+}
+
+module.exports.display = display;
+//module.export=log //exporting function only
