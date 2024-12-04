58fce30ea1c3c4ac5a7e784f0093f0e389a6d380
Sanu-Ranjan <ranjan.code33@gmail.com>
2024-12-04 23:11:10 +0530

    events module:event trigger if function called with args

--- some Inbuilt modules/event.js
+++ some Inbuilt modules/event.js
@@ -3,13 +3,13 @@ const EventEmitter = require("node:events");
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
 
-myEmitter.on("event", () => {
-  console.log("an event occurred!");
+myEmitter.on("event", (...args) => {
+  console.log("an event occurred!" + args);
 });
 
 add(3, 2);
 
 function add(num1, num2) {
-  myEmitter.emit("event");
+  myEmitter.emit("event", "a", "b");
   return num1 + num2;
 }
