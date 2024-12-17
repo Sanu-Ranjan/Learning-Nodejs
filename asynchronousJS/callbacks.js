fbf8e2c386af30300dde068ea70fc952f67466a1
Sanu-Ranjan <ranjan.code33@gmail.com>
2024-12-17 21:46:37 +0530

    AsynchronousJS- replaced call backs with promises

--- asynchronousJS/callbacks.js
+++ asynchronousJS/callbacks.js
@@ -1,26 +1,35 @@
-getUser(1, handleUserData);
+getUser(1).then(handleUserData).then(handleRepo);
 
 function handleUserData(user) {
+  //data handler or task 1
   console.log(user);
-  getRepositories("user1", handleRepo);
+  return getRepositories("user1"); //trigger task 2
 }
+
 function handleRepo(data) {
+  //data handler for  second task
   console.log(data.repo);
 }
 
 //after 2 seconds will pass data to call back function
 //this simulates asynchronous behaviour
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("reaing from database");
-    callback({ id: id, gitHubUser: "user1" });
-  }, 1000);
+function getUser(id) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      //task 1
+      console.log("reading from database");
+      resolve({ id: id, gitHubUser: "user1" });
+    }, 1000);
+  });
 }
 
 //after 2 seconds will pass data to call back function
 //this simulates asynchronous behaviour
-function getRepositories(user, callback) {
-  setTimeout(() => {
-    callback({ user: user, repo: ["repo1", "repo2", "repo3"] });
-  }, 2000);
+function getRepositories(user) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      //task2
+      resolve({ user: user, repo: ["repo1", "repo2", "repo3"] });
+    }, 2000);
+  });
 }
